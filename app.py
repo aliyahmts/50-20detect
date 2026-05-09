ec6a7dcda9faf6a30993c9a74ed64a5fb3e1f9ca
aliyahmts <s.sobradil.aliyahmargaret@cmu.edu.ph>
2026-05-09 22:42:36 +0800

    render deployment requirements

--- app.py
+++ app.py
@@ -202,4 +202,5 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
